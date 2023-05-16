5e5b05e79e6eda647a2201b5c9601905ba569137
KrasnovDenis <Elo.1972@yandex.ru>
2023-05-17 00:39:42 +0400

    metrics

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:17
 MAINTAINER Denis Krasnov
 LABEL APPLICATION=Counter
-FROM maven:3.6-openjdk-17 AS build
 
-COPY target/counter-0.0.1-SNAPSHOT.jar /usr/app/counter-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/usr/app/counter-0.0.1-SNAPSHOT.jar"]
+COPY target/counter-0.0.2-SNAPSHOT.jar /usr/app/counter-0.0.2-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/usr/app/counter-0.0.2-SNAPSHOT.jar"]
--- src/main/java/com/krasnov/counter/MetricsController.java
+++ src/main/java/com/krasnov/counter/MetricsController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 @RestController
 public class MetricsController {
@@ -12,10 +13,15 @@ public class MetricsController {
     @GetMapping("/metrics")
     public ResponseEntity<?> sendMetrics() {
         return ResponseEntity.ok(new HashMap<>() {{
-            put("Available processors", Runtime.getRuntime().availableProcessors());
-            put("Available memory", Runtime.getRuntime().freeMemory());
-            put("Total memory", Runtime.getRuntime().totalMemory());
-            put("Max memory", Runtime.getRuntime().maxMemory());
+            put("metrics", new LinkedHashMap<>() {
+                {
+                    put("Available processors", Runtime.getRuntime().availableProcessors());
+                    put("Available memory", Runtime.getRuntime().freeMemory());
+                    put("Total memory", Runtime.getRuntime().totalMemory());
+                    put("Max memory", Runtime.getRuntime().maxMemory());
+                }
+            });
+            put("application", "counter");
         }});
     }
 }
